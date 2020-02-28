3ec636792c9530afcf6554d3229aa7e2c5b06dc0
Joshua Adiel Wijaya <awe777@uw.edu>
2020-02-28 11:25:44 +0700

    added installation for COCOapi

--- install.bat
+++ install.bat
@@ -4,3 +4,7 @@ pip install matplotlib
 pip install jupyter
 pip install keract
 pip install tensorflow
+pip install scikit-image
+wget https://download.microsoft.com/download/5/f/7/5f7acaeb-8363-451f-9425-68a90f98b238/visualcppbuildtools_full.exe
+visualcppbuildtools_full.exe
+pip install git+https://github.com/philferriere/cocoapi.git#subdirectory=PythonAPI
