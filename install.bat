f101d03466cdeabf603702d7b835cdf15ad6cbd8
Joshua Adiel Wijaya <awe777@uw.edu>
2020-03-03 20:15:20 +0700

    wrap the target data model onto an iter generator

--- install.bat
+++ install.bat
@@ -5,6 +5,8 @@ pip install jupyter
 pip install keract
 pip install tensorflow
 pip install scikit-image
-wget https://download.microsoft.com/download/5/f/7/5f7acaeb-8363-451f-9425-68a90f98b238/visualcppbuildtools_full.exe
-visualcppbuildtools_full.exe
+pip install image
+:: wget https://download.microsoft.com/download/5/f/7/5f7acaeb-8363-451f-9425-68a90f98b238/visualcppbuildtools_full.exe
+:: visualcppbuildtools_full.exe
+pip install cython
 pip install git+https://github.com/philferriere/cocoapi.git#subdirectory=PythonAPI
